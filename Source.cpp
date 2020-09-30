e13ed55a28fc847ed16eea3f289e5f9358db87e3
Zailta <mannanbhat60@gmail.com>
2020-09-30 14:27:17 +0530

    Stacks STL , push(), pop(), display()

---
+++ Source.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(stack <int> st) {
+    while (!st.empty()) {
+        cout << st.top << endl;
+        st.pop();
+    }
+}
+int main() {
+    stack <int> st;
+    st.push(10);
+    st.push(10);
+    st.push(10);
+    st.push(10);
+    st.push(10);
+
+    print(st);
+    return 0;
+}
