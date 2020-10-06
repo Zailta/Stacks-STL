4d87f52920558c2979e1a3917506d66437fa0fde
Zailta <mannanbhat60@gmail.com>
2020-10-06 12:14:29 +0530

    Infix to post fix using STL in stacks

--- Source.cpp
+++ Source.cpp
@@ -1,20 +1,65 @@
-#include<bits/stdc++.h>
+#include<bits/stdc++.h> 
 using namespace std;
 
-void print(stack <int> st) {
-    while (!st.empty()) {
-        cout << st.top() << endl;
-        st.pop();
-    }
+int priority(char c) {
+    if (c == '^')
+        return 3;
+    else if (c == '*' || c == '/')
+        return 2;
+    else if (c == '+' || c == '-')
+        return 1;
+    else
+        return -1;
 }
-int main() {
-    stack <int> st;
-    st.push(10);
-    st.push(10);
-    st.push(10);
-    st.push(10);
-    st.push(10);
+void infixToPostfix(string s) {
+    stack<char> st;
+    st.push('N');
+    int l = s.length();
+    string ns;
 
-    print(st);
+    //checking for the character value in the stack
+    for (int i = 0; i < l; i++) {
+        if (s[i] > 'a' && s[i] < 'z' || s[i]>'A' && s[i] < 'Z')
+            ns = ns + s[i];
+        else if (s[i] == '(')
+            st.push('(');
+        else if (s[i] == ')') {
+            while (st.top() != 'N' && st.top() != '(') {
+                char c = st.top();
+                st.pop();
+                ns += c;
+            }
+            if (st.top() == '(') {
+                char c = st.top();
+                st.pop();
+            }
+        }
+
+        //now check priority of the operator;
+        else {
+            if (st.top() != 'N' && priority(s[i]) <= priority(st.top())) {
+                char c = st.top();
+                st.pop();
+                ns += c;
+
+            }
+            st.push(s[i]);
+        }
+    }
+    while (st.top() != 'N') {
+        char c = st.top();
+        st.pop();
+        ns += c;
+
+    }
+    cout << ns << endl;
+
+}
+int main()
+{
+    string exp = "a+b*(c^d-e)^(f+g*h)-i";
+    infixToPostfix(exp);
+
+    cout << "done";
     return 0;
 }
